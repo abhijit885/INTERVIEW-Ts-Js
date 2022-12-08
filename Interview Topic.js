82c14d9ec94be97f01c293b348362e7ecfe87b09
abhijit885 <47118840+abhijit885@users.noreply.github.com>
2022-12-08 10:44:06 +0530

    1

--- Interview Topic.js
+++ Interview Topic.js
@@ -1,1 +1,65 @@
-
+// touchableOpacity vs button
+// map vs forEach
+// for vs forEach
+// scrollView vs flatlist
+// tradutional function vs arrow function
+// Lifecycle of React Native compare with functional component
+// high resolution image optimize
+// if keystore loose what will do
+// certifecate create in importScriptsvertual scroll view
+// cashing in RN
+// null vs nan vs undefined
+// datatypes ??
+// while vs doWhile
+// spread vs rest
+// event loop
+// call shack ??
+// call back quee ??
+// microtask quee ??
+// function declaration ??
+// function expression   ??
+// IIFF ??
+// first class function ??
+// lexical scope ??
+// scope chain ??
+// first cass finction ??
+// call back function??
+// hire order function ??
+// array to string convert
+// string to array convert
+// remove dublecat elements from array and string 
+// Spread,rest,
+// ref()
+// useRef()
+// UseMemo()
+// forEach vs Map
+// var number = 42
+// console.log(number++) //42
+// console.log(number)   //43
+// console.log(++number)   //44
+// console.log(number)       //44
+// console.log(number--) //42
+// console.log(number) //41
+// console.log(--number) //41
+// console.log(number) //41
+// var num = (++number) + (number++) //43 + 43 = 86
+// var num = (number++) + (++number) //42 + 44 = 86
+// var num = (--number) + (number--) //41 + 41 = 82
+// var num = (number--) + (--number) //42 + 40 = 82
+// scope ??
+// hosting ??
+// shadowing ??
+// closer ??
+// illegal Shadowing ??
+// temporal dead zone ??
+// annonomas function ???
+// first class function ??
+// var VS let VS const
+// MAP() FILTER() REDUCE() sort() split() slice() trim()
+// Polyfill for map() FILTER() REDUCE() ??
+// testing JEST + ENZYME
+// PAYMENT GETWAY
+// MAP IN APP INTEGRATION
+// JS single threaded how perform call back fuction
+// how roted screen potret to landscrep
+// payment gatway
