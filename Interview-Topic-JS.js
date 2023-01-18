e9d8d77996b6f3be076351310700e1f29de5ccf4
abhijitCN <abhijitsaha@capitalnumbers.com>
2023-01-18 15:44:56 +0530

    #

--- Interview-Topic-JS.js
+++ Interview-Topic-JS.js
@@ -87,3 +87,99 @@
 
 // console.log(beasts.indexOf('giraffe'));
 // // expected output: -1
+// no changes in main array go thtough all item if true execution stop and return
+// no changes in main array go thtough all item if true execution stop and return
+var num = [1, 3, 8, 6, 7, 4, 6]
+var result = num.find((item, index, arr) => {
+    return item > 3
+}, this)
+console.log(result)
+//if not find return -1,try to find any element exist in arr main arr unchanged
+var num2 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result2 = num2.findIndex((item, index, arr) => {
+    //return item > 6
+    return !(item % 2)
+})
+console.log(result2)
+// return a array , not modifi original array , if nothing exist return blank []
+var num3 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result3 = num3.filter((item, index, arr) => {
+    return item > 30
+})
+console.log(result3)
+// ctart and end is the position mot index ,if not find return blank []
+const num4 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result4 = num4.slice(-3, 8)
+console.log(result4)
+// modify the existing array and return which are deleted frome array , (start,how many item will delete,added elements)
+const num5 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result5 = num5.splice(2, 2, 22, 33)
+console.log(result5)
+console.log(num5)
+//original array unchanged , add 2 array 
+const num6 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const num66 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result6 = num6.concat(num66)
+console.log(result6)
+// add some element end of array ,change in original array 
+const num7 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result7 = num7.push(10000)
+const result77 = num7.push([100, 200])
+console.log(num7)
+console.log(result77)
+//like for loop , loop through a array and operation on every element it , original array unchanged,return array
+const num8 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result8 = num8.map((item) => {
+    return item * 2
+})
+console.log(result8)
+// oiginal array unchanged,return a single value,(previous,current,currentIndex,arr) , 2nd peramiter where you want to start from
+const num9 = [1, 2, 3, 8, 6, 7, 4, 6, 2]
+const result9 = num9.reduce((previous, current, currentIndex, arr) => {
+    return previous + current
+}, 0)
+console.log(result9)
+//for in loop operate in object
+const objEx = {
+    name: 'abh',
+    roll: 12,
+    address: 'kolkata'
+}
+for (property in objEx) {
+    console.log(property + 2)
+}
+// for of any etarable object like array,object,string
+const arrEx = [5, 6, 4, 9, 2, 7, 3]
+for (element of arrEx) {
+    console.log(element + 2)
+}
+//for in etaret all elements and return the index if the value
+const arrEx2 = [5, 6, 4, 9, 2, 7, 3]
+for (element in arrEx2) {
+    console.log(element)
+}
+//for of etaret all elements and return the value
+const arrEx3 = 'I LOVE CODE'
+for (element2 of arrEx3) {
+    console.log(element2)
+}
+//object methods keys return a array wth the object keys,values return a array wth the object values,entries return a array wth the object keys value pare in a array of array,
+const objEx4 = {
+    name: 'abh',
+    roll: 12,
+    address: 'kolkata'
+}
+const keys = Object.keys(objEx4)
+const values = Object.values(objEx4)
+const entries = Object.entries(objEx4)
+console.log(keys)
+console.log(values)
+console.log(entries)
+// es6 default perameter
+const defaultParam = (x = 2) => {
+    return x
+}
+console.log("ww", defaultParam())
+
+
+
