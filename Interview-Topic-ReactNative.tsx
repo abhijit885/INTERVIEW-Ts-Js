f144391c2ea5aca993501d926c61fc8a60f53eaf
abhijitCN <abhijitsaha@capitalnumbers.com>
2023-02-23 16:43:59 +0530

    0

--- Interview-Topic-ReactNative.tsx
+++ Interview-Topic-ReactNative.tsx
@@ -3,7 +3,8 @@
 //Xcode version : 12.4
 //@react-navigation/native : V5 ,V6
 // netive app vs hybrid app
-
+//how to reduce the app size? 
+//how to improve react native app performance 
 // what is safeAreaView : SafeAreaView renders nested content and automatically applies padding to reflect the portion of the view that is not covered by navigation bars, tab bars, toolbars, and other ancestor views. Moreover, and most importantly, Safe Area's paddings reflect the physical limitation of the screen, such as rounded corners or camera notches (i.e. the sensor housing area on iPhone 13).
 
 // yarn VS npm
